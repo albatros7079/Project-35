1956e1dce27d45860f64dc9c6ae30429b2cfe4de
albatross7079 <70518748+albatros7079@users.noreply.github.com>
2020-11-03 16:46:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,7 +46,13 @@ function draw(){
   
  fill(255,255,254);
  textSize(15);
-
+if(lastFed>=12) {
+text("Last Feed : "+ lastFed%12 + "PM", 350,30);
+}else if(lastFed==-0){
+text("Last Feed : 12 AM", 350,30);c
+}else{
+text("Last Feed : "+lastFed+"AM",350,30)
+}
 drawSprites();
 }
 function readPosition(data){
